272b5f1d784a289c998582d64260e7823ca0a306
Aydar <bilalovaidarhaidarovich@yandex.ru>
2019-07-24 23:55:54 +0500

    Задание 1 3го урока

---
+++ src/lesson3/main.java
@@ -0,0 +1,20 @@
+package lesson3;
+
+public class main {
+    public static void main(String[] args) {
+        String[] strArr = {"яблоко","груша","апельсин","мандарин","вишня","черешня","яблоко","вишня","малина","арбуз","арбуз","черника","крыжовник","смородина","земляника","грейпфрут"};
+        returnWithout(strArr);
+
+    }
+
+    private static void returnWithout(String[] strArr) {
+        for (int i = 0; i < strArr.length; i++) {
+            for (int j = 0; j < strArr.length; j++) {
+                if ((strArr[i] == strArr[j]) && (j > i || j < i)) {
+                    break;}
+                else if (strArr[i] == strArr[j])
+                    System.out.println(strArr[i]);
+            }
+        }
+    }
+}
